4f5c807c7b5099ccedfd704be3745e7fcf175090
Silenthowler <23553428+domcrouse@users.noreply.github.com>
2020-12-03 14:03:22 +0000

    ML-Agents-Upload

---
+++ Machine learning (pengiuns)/Assets/Assets/Penguins/Scripts/HunterAgent.cs
@@ -0,0 +1,147 @@
+﻿
+using UnityEngine;
+using MLAgents;
+using System.ComponentModel;
+
+public class HunterAgent : Agent
+{
+    [Tooltip("How fast the agent moves forward")]
+    public float moveSpeed = 5f;
+
+    [Tooltip("How fast the agent turns")]
+    public float turnSpeed = 120f;
+
+    [Tooltip("Prefab of the heart that appears when the baby is fed")]
+    public GameObject heartPrefab;
+
+    [Tooltip("Prefab of the regurgitated fish that appears when the baby is fed")]
+    public GameObject regurgitatedFishPrefab;
+
+    private PenguinArea penguinArea;
+    new private Rigidbody rigidbody;
+    private GameObject baby;
+    private bool isFull; // If true, penguin has a full stomach
+    private float feedRadius = 0f;
+
+
+    public override void InitializeAgent()
+    {
+        base.InitializeAgent();
+        penguinArea = GetComponentInParent<PenguinArea>();
+        baby = penguinArea.penguinBaby;
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    /// <summary>
+    /// Perform actions based on a vector of numbers
+    /// </summary>
+    /// <param name="vectorAction">The list of actions to take</param>
+    public override void AgentAction(float[] vectorAction)
+    {
+        // Convert the first action to forward movement
+        float forwardAmount = vectorAction[0];
+
+        // Convert the second action to turning left or right
+        float turnAmount = 0f;
+        if (vectorAction[1] == 1f)
+        {
+            turnAmount = -1f;
+        }
+        else if (vectorAction[1] == 2f)
+        {
+            turnAmount = 1f;
+        }
+
+        // Apply movement
+        rigidbody.MovePosition(transform.position + transform.forward * forwardAmount * moveSpeed * Time.fixedDeltaTime);
+        transform.Rotate(transform.up * turnAmount * turnSpeed * Time.fixedDeltaTime);
+
+        // Apply a tiny negative reward every step to encourage action
+        if (maxStep > 0) AddReward(-1f / maxStep);
+    }
+
+    public override float[] Heuristic()
+    {
+        float forwardAction = 0f;
+        float turnAction = 0f;
+        if (Input.GetKey(KeyCode.W))
+        {
+            // move forward
+            forwardAction = 1f;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            // turn left
+            turnAction = 1f;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            // turn right
+            turnAction = 2f;
+        }
+        // Put the actions into an array and return
+        return new float[] { forwardAction, turnAction };
+    }
+
+    public override void AgentReset()
+    {
+        isFull = false;
+        penguinArea.ResetArea();
+        feedRadius = Academy.Instance.FloatProperties.GetPropertyWithDefault("feed_radius", 0f);
+    }
+
+    public override void CollectObservations()
+    {
+        // Whether the penguin has eaten a fish (1 float = 1 value)
+        AddVectorObs(isFull);
+
+        // Distance to the baby (1 float = 1 value)
+        AddVectorObs(Vector3.Distance(baby.transform.position, transform.position));
+
+        // Direction to baby (1 Vector3 = 3 values)
+        AddVectorObs((baby.transform.position - transform.position).normalized);
+
+        // Direction penguin is facing (1 Vector3 = 3 values)
+        AddVectorObs(transform.forward);
+
+        //Distance form the hunter (1 float = 1 value)
+        AddVectorObs(Vector3.Distance(hunter.transform.position, transform.position));
+
+        //Direction to the hunter (1 Vecotr = 3 Values)
+        AddVectorObs((hunter.transform.position - transform.position).normalized);
+
+
+        // 1 + 1 + 3 + 3 = 8 total values
+    }
+
+    private void FixedUpdate()
+    {
+        // Request a decision every 5 steps. RequestDecision() automatically calls RequestAction(),
+        // but for the steps in between, we need to call it explicitly to take action using the results
+        // of the previous decision
+        if (GetStepCount() % 5 == 0)
+        {
+            RequestDecision();
+        }
+        else
+        {
+            RequestAction();
+        }
+
+        // Test if the agent is close enough to to feed the baby
+        if (Vector3.Distance(transform.position, baby.transform.position) < feedRadius)
+        {
+            // Close enough, try to feed the baby
+            RegurgitateFish();
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Penguin")
+        {
+            AddReward(2f);
+        }
+        
+    }
+}
